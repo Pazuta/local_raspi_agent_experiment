f22c5a822d458d24183c8731e71707899d352543
Pazuta <t.beier233@gmail.com>
2026-09-19 20:57:36 +0200

    add Docker support, remove nginx

--- ai.py
+++
@@ -1,9 +0,0 @@
-from app import generate_reply
-
-
-def LLM(prompt):
-    return generate_reply(prompt)
-
-if __name__ == "__main__":
-    initial_prompt = "User has not added any prompt."
-    print(LLM(initial_prompt))
